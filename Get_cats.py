7c4b959aa3bd4a932e0f398ff4e57d31273674e3
dm88i <dm88ik@gmail.com>
2024-10-15 16:13:50 +0700

    Написал функцию new_get_img

--- Get_cats.py
+++ Get_cats.py
@@ -9,19 +9,26 @@ def get_cat(url):
     image = BytesIO(info_image.content)
     img = Image.open(image)
     img_tk = ImageTk.PhotoImage(img)
-    img.thumbnail(500,400)
+    img.thumbnail((500, 400))
     return img_tk
+
+def new_get_img():
+    img = get_cat(url)
+    if img:
+        t_m.config(image=img)
+        t_m.image = img
 
 window = Tk()
 window.title('Cats')
-window.geometry(f'500x400+{window.winfo_screenwidth()//2-250}+{window.winfo_screenheight()//2-200}')
+window.geometry(f'500x440+{window.winfo_screenwidth()//2-250}+{window.winfo_screenheight()//2-200}')
 window.iconbitmap('cat_icons.ico')
 url = 'https://cataas.com/cat'
-img = get_cat(url)
-t_m = Label(window, image=img)
 
-btn = Button(window, text='Получить кота')
+t_m = Label(window)
 t_m.pack()
+
+btn = Button(window, text='Получить кота', command=new_get_img)
+btn.pack()
 
 
 
