964cbe8c7424d5d42b5b10cf56f0c06894770cd1
dm88i <dm88ik@gmail.com>
2024-10-15 16:24:04 +0700

    Создал новое окно

--- Get_cats.py
+++ Get_cats.py
@@ -15,21 +15,31 @@ def get_cat(url):
 def new_get_img():
     img = get_cat(url)
     if img:
-        t_m.config(image=img)
+        new_win = Toplevel()
+        t_m = Label(new_win, image=img)
         t_m.image = img
+        t_m.pack()
+
+
+
+def exit_win():
+    window.destroy()
 
 window = Tk()
 window.title('Cats')
-window.geometry(f'500x440+{window.winfo_screenwidth()//2-250}+{window.winfo_screenheight()//2-200}')
+window.geometry(f'200x140+{window.winfo_screenwidth()//2-250}+{window.winfo_screenheight()//2-200}')
 window.iconbitmap('cat_icons.ico')
 url = 'https://cataas.com/cat'
 
-t_m = Label(window)
-t_m.pack()
 
-btn = Button(window, text='Получить кота', command=new_get_img)
-btn.pack()
+main_menu = Menu(window)
+window.config(menu=main_menu)
+file_menu = Menu(main_menu, tearoff=0)
 
+main_menu.add_cascade(label='Файл', menu=file_menu)
+file_menu.add_command(label='Получить кота', command=new_get_img)
+file_menu.add_separator()
+file_menu.add_command(label='Выход', command=exit_win)
 
 
 
